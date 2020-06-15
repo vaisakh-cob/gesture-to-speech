689b8220aceb96c2fc531d61df31557119e49911
Pishu <vaisakh.pisharody@factset.com>
2020-06-15 16:41:20 +0530

    Runtime Error Fixes 1. Changed the path in config updater files

--- lib/gesture_phrase_updater.py
+++ lib/gesture_phrase_updater.py
@@ -1,10 +1,13 @@
 import configparser
 import sys
 
-gesture_phrase_map = sys.argv[1].split(",")
+gesture_phrase_map = sys.argv[1]
 
-config_path = "config\\config.ini"
+config_path = "..\\config\\config.ini"
 
 config = configparser.ConfigParser()
+config.read(config_path)
+config[gesture_phrase_map[0]]["EMERGENCY_MODE"] = gesture_phrase_map[1]
+
 with open(config_path, 'w') as configfile:
-    config._write_section(configfile, gesture_phrase_map[0], "EMERGENCY_MODE", gesture_phrase_map[1])
+    config.write(configfile)
